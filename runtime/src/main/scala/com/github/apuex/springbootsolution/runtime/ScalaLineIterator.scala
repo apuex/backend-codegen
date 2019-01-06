88e2cb23c42f798e2571d42f5a91afdcf83a77c6
Wangxy <xtwxy@hotmail.com>
2019-01-06 17:42:39 +0800

    add patch for scala input stream line iterator.

---
+++ runtime/src/main/scala/com/github/apuex/springbootsolution/runtime/ScalaLineIterator.scala
@@ -0,0 +1,40 @@
+package com.github.apuex.springbootsolution.runtime
+
+import java.io.{BufferedReader, IOException, UncheckedIOException}
+import java.util.NoSuchElementException
+
+
+/**
+  * This is a patch for scala line iterator, which is subject to change.
+  *
+  * Since the following approach is unstable because a bug exists in the
+  * Scala standard library, we created this class to use java alternative
+  * as walk-around.
+  *
+  * Source.fromFile("my-file.txt")
+  *   .map(line -> ...)
+  *   ...
+  *
+  * @param reader
+  */
+class ScalaLineIterator(reader: BufferedReader) extends Iterator[String] {
+  var nextLine: String = null
+
+  override def hasNext: Boolean = if (nextLine != null) return true
+  else try {
+    nextLine = reader.readLine
+    return nextLine != null
+  } catch {
+    case e: IOException =>
+      throw new UncheckedIOException(e)
+  }
+
+  override def next(): String = {
+    if (nextLine != null || hasNext) {
+      val line = nextLine
+      nextLine = null
+      return line
+    }
+    else throw new NoSuchElementException
+  }
+}
