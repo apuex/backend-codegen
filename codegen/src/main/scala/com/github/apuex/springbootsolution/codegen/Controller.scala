f7184a0938b4c73a917dd6899539a6ad7ef79012
Wangxy <xtwxy@hotmail.com>
2019-03-20 17:36:37 +0800

    add export to excel gen.

--- codegen/src/main/scala/com/github/apuex/springbootsolution/codegen/Controller.scala
+++ codegen/src/main/scala/com/github/apuex/springbootsolution/codegen/Controller.scala
@@ -2,7 +2,9 @@ package com.github.apuex.springbootsolution.codegen
 
 import java.io.{File, PrintWriter}
 
+import com.github.apuex.springbootsolution.codegen.ModelUtils.persistentColumnsExtended
 import com.github.apuex.springbootsolution.runtime.SymbolConverters._
+import com.github.apuex.springbootsolution.runtime.TextUtils._
 
 import scala.xml._
 
@@ -25,17 +27,26 @@ object Controller extends App {
     .foreach(x => controllerForEntity(modelPackage, x))
 
   private def controllerForEntity(modelPackage: String, entity: Node): Unit = {
-    val entityName = entity.attribute("name").asInstanceOf[Some[Text]].get.data
+    val entityName = entity.\@("name")
+    val columns = persistentColumnsExtended(xml, entity)
+        .map(f => (f.\@("name"), f.\@("type")))
+
     val prelude =
     s"""package ${modelPackage}.controller;
       |
       |import com.github.apuex.springbootsolution.runtime.*;
+      |import static com.github.apuex.springbootsolution.runtime.DateFormat.*;
       |import ${modelPackage}.message.*;
       |import ${modelPackage}.service.*;
+      |import org.apache.poi.hssf.usermodel.*;
+      |import org.apache.poi.hssf.util.*;
+      |import org.apache.poi.ss.usermodel.*;
       |import org.springframework.beans.factory.annotation.*;
       |import org.springframework.web.bind.annotation.*;
       |import javax.servlet.http.*;
       |import java.net.*;
+      |import java.io.*;
+      |import java.util.*;
       |
       |@RestController
       |@RequestMapping(value="${cToShell(entityName)}", method=RequestMethod.POST)
@@ -67,6 +78,43 @@ object Controller extends App {
       |    return service.query(q, r.getUserPrincipal(), new URI(r.getRequestURI()));
       |  }
       |
+      |  @RequestMapping(value="${cToShell("%s%s%s".format("export", hyphen, cToShell(entityName)))}", consumes="application/json")
+      |  public void export(@RequestBody QueryCommand q, HttpServletRequest request, HttpServletResponse response) throws URISyntaxException, IOException {
+      |    final ${cToPascal(entityName)}ListVo listVo = service.query(q, request.getUserPrincipal(), new URI(request.getRequestURI()));
+      |    HSSFWorkbook wb = new HSSFWorkbook();
+      |    HSSFSheet sheet = wb.createSheet("${cToPascal(entityName)}");
+      |
+      |    HSSFCellStyle style = wb.createCellStyle();
+      |    style.setFillBackgroundColor(HSSFColor.HSSFColorPredefined.AQUA.getIndex());
+      |    style.setFillPattern(FillPatternType.BIG_SPOTS);
+      |
+      |    style = wb.createCellStyle();
+      |    style.setFillForegroundColor(HSSFColor.HSSFColorPredefined.ORANGE.getIndex());
+      |    style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+      |
+      |    short rowNumber = 1;
+      |    exportHeaderCells(sheet.createRow(rowNumber++), style);
+      |    for(${cToPascal(entityName)}Vo vo: listVo.getItemsList()) {
+      |      HSSFRow row = sheet.createRow(rowNumber++);
+      |      exportDataCells(vo, row, style);
+      |    }
+      |
+      |    response.setContentType("application/vnd.ms-excel");
+      |    response.setHeader("Content-disposition",String.format("attachment; filename=%sList-%s.xls", "${cToPascal(entityName)}", formatTimestamp(new Date())));
+      |    wb.write(response.getOutputStream());
+      |  }
+      |
+      |  private void exportHeaderCells(HSSFRow row, HSSFCellStyle style) {
+      |    short colNumber = 1;
+      |    HSSFCell cell = null;
+      |    ${indent(headerCells(columns), 4)}
+      |  }
+      |
+      |  private void exportDataCells(${cToPascal(entityName)}Vo vo, HSSFRow row, HSSFCellStyle style) {
+      |    short colNumber = 1;
+      |    HSSFCell cell = null;
+      |    ${indent(dataCells(columns), 4)}
+      |  }
       |""".stripMargin
 
     val end =
@@ -80,6 +128,20 @@ object Controller extends App {
 
     printWriter.close()
   }
+
+  private def headerCells(columns: Seq[(String, String)]): String = columns
+    .map(x => String.format("cell = row.createCell(colNumber);\ncell.setCellValue(\"%s\");\ncell.setCellStyle(style);", cToPascal(x._1)))
+    .reduce((x, y) => "%s\n%s".format(x, y))
+
+  private def dataCells(columns: Seq[(String, String)]): String = columns
+    .map(x => String.format("cell = row.createCell(colNumber);\ncell.setCellValue(%s);\ncell.setCellStyle(style);", formatCell(x._1, x._2)))
+    .reduce((x, y) => "%s\n%s".format(x, y))
+
+  private def formatCell(name: String, t: String): String =
+    if(t.eq("timestamp"))
+      s"""String.format("%s", formatTimestamp(vo.get${cToPascal(name)}()))"""
+    else
+      s"""String.format("%s", vo.get${cToPascal(name)}())"""
 
   private def project = {
     val printWriter = new PrintWriter(s"${projectDir}/pom.xml", "utf-8")
@@ -103,8 +165,18 @@ object Controller extends App {
          |  <dependencies>
          |    <dependency>
          |      <groupId>${modelPackage}</groupId>
+         |      <artifactId>${cToShell(modelName)}-message</artifactId>
+         |      <version>1.0-SNAPSHOT</version>
+         |    </dependency>
+         |    <dependency>
+         |      <groupId>${modelPackage}</groupId>
          |      <artifactId>${cToShell(modelName)}-service</artifactId>
          |      <version>1.0-SNAPSHOT</version>
+         |    </dependency>
+         |    <dependency>
+         |      <groupId>com.github.apuex.springbootsolution</groupId>
+         |      <artifactId>runtime_2.12</artifactId>
+         |      <version>1.0.6</version>
          |    </dependency>
          |    <dependency>
          |      <groupId>org.springframework.boot</groupId>
@@ -116,6 +188,16 @@ object Controller extends App {
          |      <artifactId>spring-boot-starter-test</artifactId>
          |      <version>2.0.3.RELEASE</version>
          |      <scope>test</scope>
+         |    </dependency>
+         |    <dependency>
+         |      <groupId>org.apache.poi</groupId>
+         |      <artifactId>poi</artifactId>
+         |      <version>3.17</version>
+         |    </dependency>
+         |    <dependency>
+         |      <groupId>org.apache.poi</groupId>
+         |      <artifactId>poi-ooxml</artifactId>
+         |      <version>3.17</version>
          |    </dependency>
          |    <dependency>
          |      <groupId>com.jayway.jsonpath</groupId>
