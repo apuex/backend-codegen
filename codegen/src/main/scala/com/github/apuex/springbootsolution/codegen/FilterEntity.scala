00a4b1c7b1020ffe5b0cfe72d6460a17307b1a52
Wangxy <xtwxy@hotmail.com>
2019-05-26 13:08:03 +0800

    implement xml filter.

--- codegen/src/main/scala/com/github/apuex/springbootsolution/codegen/FilterEntity.scala
+++ codegen/src/main/scala/com/github/apuex/springbootsolution/codegen/FilterEntity.scala
@@ -1,6 +1,6 @@
 package com.github.apuex.springbootsolution.codegen
 
-import java.io.{File, PrintWriter}
+import java.io.{ File, PrintWriter }
 
 import com.github.apuex.springbootsolution.runtime.SymbolConverters._
 
@@ -8,11 +8,23 @@ import scala.xml.Text
 import scala.xml.parsing._
 import scala.xml._
 
-
 object FilterEntity extends App {
   val xml = ModelLoader(args(0)).xml
-  xml.child
-  .filter(x => x.label == "entity")
-  .filter(x => !(x.child.filter(c => c.label == "primaryKey").isEmpty))
-  .foreach(println)
+
+  println(xml.getClass.getName)
+  val root = xml.asInstanceOf[Elem]
+    .copy(
+      prefix = xml.prefix,
+      label = xml.label, //val label: String,
+      attributes = xml.attributes, //: MetaData,
+      scope = xml.scope,
+      minimizeEmpty = true,
+      child = xml.child
+        .filter(x => x.label == "entity")
+        .filter(x =>
+          !(x.child.filter(c => c.label == "primaryKey").isEmpty)
+            || !(x.child.filter(c => c.label == "uniqueKey").isEmpty))
+        .toSeq)
+
+  println(root)
 }
