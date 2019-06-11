168f736610d623013208d21084e7388f8d13457d
Wangxy <xtwxy@hotmail.com>
2019-06-11 14:44:55 +0800

    implement auxiliary methods for building QueryCommand(s).

---
+++ scala-runtime/src/main/scala/com/github/apuex/springbootsolution/runtime/QueryCommandMethods.scala
@@ -0,0 +1,45 @@
+package com.github.apuex.springbootsolution.runtime
+
+import java.util.Date
+
+import com.github.apuex.springbootsolution.runtime.FilterPredicate.Clause.{Connection, Predicate}
+import com.github.apuex.springbootsolution.runtime.LogicalConnectionType.AND
+import com.google.protobuf.Timestamp
+import com.google.protobuf.timestamp
+import com.google.protobuf.util.Timestamps
+
+object QueryCommandMethods {
+
+  def andCommand(params: Map[String, Any]): QueryCommand = {
+    QueryCommand(
+      Some(
+        FilterPredicate(
+          Connection(
+            LogicalConnectionVo(
+              AND,
+              params.keys.map(equalsPredicate(_)).toSeq
+            )
+          )
+        )
+      ),
+      params.map(x => (x._1 -> stringValue(x._2)))
+    )
+  }
+
+  def equalsPredicate(name: String): FilterPredicate = FilterPredicate(
+    Predicate(
+      LogicalPredicateVo(
+        PredicateType.EQ,
+        name,
+        Seq(name)
+      )
+    )
+  )
+
+  def stringValue: Any => String = {
+    case x: Date => Timestamps.toString(Timestamps.fromMillis(x.getTime))
+    case x: Timestamp => Timestamps.toString(x)
+    case x: timestamp.Timestamp => Timestamps.toString(timestamp.Timestamp.toJavaProto(x))
+    case x => x.toString
+  }
+}
