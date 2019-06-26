3906dae0d04b356f4e8747f5f57291c2f50056df
Wangxy <xtwxy@hotmail.com>
2019-06-26 16:00:38 +0800

    add block quote utility.

--- util/src/main/scala/com/github/apuex/springbootsolution/runtime/TextUtils.scala
+++ util/src/main/scala/com/github/apuex/springbootsolution/runtime/TextUtils.scala
@@ -18,4 +18,48 @@ object TextUtils {
       }
     }
   }
+
+  def indentWithRightMargin(lines: String, spaces: Int, indentFirstLine: Boolean = false): String = {
+    if(null == lines) null
+    else {
+      val indenting = s"${(0 until spaces).map(_ => " ").foldLeft("")(_ + _)}|"
+      val indented = lines.split("[\n|\r]")
+        .map(l => "%s%s".format(indenting, l))
+        .map(x => if("" == x.trim) "" else x)
+        .reduceOption((x, y) => "%s\n%s".format(x, y))
+        .getOrElse("")
+      if(indentFirstLine) {
+        indented
+      } else {
+        if(indented.length > spaces + 1) indented.substring(spaces + 1)
+        else indented
+      }
+    }
+  }
+  
+  def indentWithLeftMargin(lines: String, spaces: Int, indentFirstLine: Boolean = false): String = {
+    if(null == lines) null
+    else {
+      val indenting = s"|${(0 until spaces).map(_ => " ").foldLeft("")(_ + _)}|"
+      val indented = lines.split("[\n|\r]")
+        .map(l => "%s%s".format(indenting, l))
+        .map(x => if("" == x.trim) "" else x)
+        .reduceOption((x, y) => "%s\n%s".format(x, y))
+        .getOrElse("")
+      if(indentFirstLine) {
+        indented
+      } else {
+        if(indented.length > spaces + 1) indented.substring(spaces + 1)
+        else indented
+      }
+    }
+  }
+  
+  def blockQuote(lines: String, spaces: Int, indentFirstLine: Boolean = false): String = {
+    s"""
+       |s\"\"\"
+       |   ${indentWithLeftMargin(indent(lines, spaces, indentFirstLine), 3)}
+       | \"\"\".stripMargin.trim
+     """.stripMargin.trim
+  }
 }
---
+++ util/src/test/scala/com/github/apuex/springbootsolution/runtime/TextUtilsSpec.scala
@@ -0,0 +1,84 @@
+package com.github.apuex.springbootsolution.runtime
+import com.github.apuex.springbootsolution.runtime.TextUtils._
+
+import org.scalatest._
+
+class TextUtilsSpec extends FlatSpec with Matchers{
+  "A indent function" should "indent lines" in {
+    val lines =
+      s"""
+         |hello,
+         |world!
+       """.stripMargin.trim
+
+    val indentSkipFirst = indent(lines, 2)
+    val indentFirst = indent(lines, 2, true)
+
+    indentSkipFirst should be(
+      s"""hello,
+         |  world!""".stripMargin
+    )
+    indentFirst should be(
+      s"""  hello,
+         |  world!""".stripMargin
+    )
+  }
+
+  "A indentWithRightMargin function" should "indent lines" in {
+    val lines =
+      s"""
+         |hello,
+         |world!
+       """.stripMargin.trim
+
+    val indentSkipFirst = indentWithRightMargin(lines, 2)
+    val indentFirst = indentWithRightMargin(lines, 2, true)
+
+    indentSkipFirst should be(
+      s"""hello,
+         |  |world!""".stripMargin
+    )
+    indentFirst should be(
+      s"""|  |hello,
+         |  |world!""".stripMargin
+    )
+  }
+
+  "A indentWithLeftMargin function" should "indent lines" in {
+    val lines =
+      s"""
+         |hello,
+         |world!
+       """.stripMargin.trim
+
+    val indentSkipFirst = indentWithLeftMargin(lines, 2)
+    val indentFirst = indentWithLeftMargin(lines, 2, true)
+
+    indentSkipFirst should be(
+      s"""||hello,
+          ||  |world!""".stripMargin
+    )
+    indentFirst should be(
+      s"""||  |hello,
+          ||  |world!""".stripMargin
+    )
+  }
+
+  "A blockQuote function" should "block quote lines" in {
+    val lines =
+      s"""
+         |hello,
+         |world!
+       """.stripMargin.trim
+
+    val indentSkipFirst = blockQuote(lines, 2)
+    val indentFirst = blockQuote(lines, 2, true)
+
+    indentSkipFirst should be(
+      "s\"\"\"\n   |hello,\n   |  world!\n \"\"\".stripMargin.trim"
+    )
+    indentFirst should be(
+      "s\"\"\"\n   |  hello,\n   |  world!\n \"\"\".stripMargin.trim"
+    )
+  }
+}
